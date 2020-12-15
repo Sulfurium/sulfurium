9daa840f4d632fc21065806dd53a815aee86a268
Woomy4680-exe <matzar1808@gmail.com>
2020-12-15 13:14:34 +0100

    :hammer: Added individual pack script

---
+++ pack.sh
@@ -0,0 +1,27 @@
+#!/usr/bin/env bash
+ROOT=`dirname $0`
+cd $ROOT
+if [[ -d "${ROOT}/build" ]]
+then
+	rm -rf "${ROOT}/build"
+fi
+mkdir "${ROOT}/build"
+if [[ -d "${ROOT}/temp" ]] 
+then
+	rm -rf "${ROOT}/temp"
+fi
+mkdir "${ROOT}/temp"
+if [[ -n "${1}" ]]
+then
+	if [[ -d "${ROOT}/packages/${1}/" ]]
+	then
+		if [[ ! -x "${ROOT}/packages/${1}/build.sh" ]]
+		then
+			echo "Missing build script for ${1}!"
+			exit 1
+		fi
+		"${ROOT}/packages/${1}/build.sh" $PWD
+		exit
+	fi
+fi
+
