bd756c33a545de296c58b0b8c4f2b80bbe80ca62
Woomy4680-exe <matzar1808@gmail.com>
2020-12-15 17:18:26 +0100

    :hammer: Added ZSTD script

---
+++ packages/zstd/build.sh
@@ -0,0 +1,40 @@
+#!/usr/bin/env bash
+ROOT=$1
+NAME="zstd"
+VER="1.4.5"
+EXT="tar.gz"
+URL="https://github.com/facebook/zstd/releases/download/v${VER}/${NAME}-${VER}.${EXT}"
+if [[ ! -d "${ROOT}/temp" ]]
+then
+    exit 1
+fi
+if [[ -d "${ROOT}/temp/${NAME}" ]]
+then
+	rm -rf "${ROOT}/temp/${NAME}"
+fi
+mkdir "${ROOT}/temp/${NAME}"
+TMP="${ROOT}/temp/${NAME}"
+mkdir "${TMP}/sources"
+mkdir "${TMP}/build"
+ARCHIVE="${NAME}-${VER}.${EXT}"
+wget "${URL}" -O "${TMP}/${ARCHIVE}" || exit 1
+tar -xf "${TMP}/${ARCHIVE}" -C "${TMP}/sources" || exit 1
+SRC_DIR="${TMP}/sources/${NAME}-${VER}"
+cd ${SRC_DIR}
+make -j $(nproc)
+mkdir -p $TMP/build/usr
+make prefix=$TMP/build/usr DESTDIR=$TMP/build/ install
+cd ${ROOT}
+# Define .PKG vars
+DATE=`date +"%D-%H:%M"`
+PACKAGER=$USER
+echo "name = \"${NAME}\"" | tee -a "${TMP}/build/.PKG"
+echo "version = $VER" | tee -a "${TMP}/build/.PKG"
+echo "subversion = 1" | tee -a "${TMP}/build/.PKG"
+echo "architecture = \"x64\"" | tee -a "${TMP}/build/.PKG"
+echo "packager = \"$USER\"" | tee -a "${TMP}/build/.PKG"
+echo "url = \"${URL}\"" | tee -a "${TMP}/build/.PKG"
+touch "${TMP}/build/.INSTALL"
+cd "${TMP}/build"
+tar -czf "${ROOT}/build/${NAME}-${VER}.tar.zst" .
+cd "${ROOT}"
