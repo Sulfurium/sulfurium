dc62c4e238fc1b656924fcd4423b539eb396718e
Woomy4680-exe <matzar1808@gmail.com>
2020-12-16 18:33:30 +0100

    :hammer: Add binutils build script

---
+++ packages/binutils/build.sh
@@ -0,0 +1,53 @@
+#!/usr/bin/env bash
+ROOT=$1
+NAME="binutils"
+VER="2.35"
+EXT="tar.xz"
+URL="http://ftp.gnu.org/gnu/binutils/${NAME}-${VER}.${EXT}"
+if [[ ! -d "${ROOT}/temp" ]]
+then
+    exit 1
+fi
+if [[ -d "${ROOT}/temp/${NAME}" ]]
+then
+	rm -rf "${ROOT}/temp/${NAME}"
+fi
+mkdir "${ROOT}/temp/${NAME}"
+TMP="${ROOT}/temp/${NAME}"
+mkdir "${TMP}/sources"
+mkdir "${TMP}/build"
+ARCHIVE="${NAME}-${VER}.${EXT}"
+wget "${URL}" -O "${TMP}/${ARCHIVE}" || exit 1
+tar -xf "${TMP}/${ARCHIVE}" -C "${TMP}/sources" || exit 1
+SRC_DIR="${TMP}/sources/${NAME}-${VER}"
+cd ${SRC_DIR}
+
+sed -i '/@\tincremental_copy/d' gold/testsuite/Makefile.in
+mkdir build
+cd build
+mkdir $TMP/build/usr
+../configure --prefix=/usr       \
+             --enable-gold       \
+             --enable-ld=default \
+             --enable-plugins    \
+             --enable-shared     \
+             --disable-werror    \
+             --enable-64-bit-bfd \
+             --with-system-zlib
+
+make tooldir=$TMP/build/usr -j $(nproc)
+make tooldir=$TMP/build/usr install DESTDIR="$TMP/build"
+cd ${ROOT}
+# Define .PKG vars
+DATE=`date +"%D-%H:%M"`
+PACKAGER=$USER
+echo "name = \"${NAME}\"" | tee -a "${TMP}/build/.PKG"
+echo "version = $VER" | tee -a "${TMP}/build/.PKG"
+echo "subversion = 1" | tee -a "${TMP}/build/.PKG"
+echo "architecture = \"x64\"" | tee -a "${TMP}/build/.PKG"
+echo "packager = \"$USER\"" | tee -a "${TMP}/build/.PKG"
+echo "url = \"${URL}\"" | tee -a "${TMP}/build/.PKG"
+touch "${TMP}/build/.INSTALL"
+cd "${TMP}/build"
+tar -czf "${ROOT}/build/${NAME}-${VER}.tar.zst" .
+cd "${ROOT}"
