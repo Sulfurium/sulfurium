4b6c00518eaeaff2dd860550d34f550971f16134
Woomy4680-exe <matzar1808@gmail.com>
2021-01-12 11:47:56 +0100

    ✨ Add dependencies compilation

--- pack.sh
+++ pack.sh
@@ -18,18 +18,52 @@ then
 			echo "Missing build script for ${1}!"
 			exit 1
 		fi
-		"${ROOT}/packages/${1}/build.sh" $PWD
-		exit
+		PKG="${ROOT}/packages/${1}"
+		if [[ -f "${PKG}/DEPENDS" ]]
+		then
+			source "${PKG}/DEPENDS"
+			for DEP in "${DEPS[*]}" 
+			do
+				if [[ ! -x "${ROOT}/packages/${DEP}/build.sh" ]]
+				then
+					echo -e "\e[91mMissing build script for ${DEP}"
+					exit 1
+				fi
+				"${ROOT}/packages/${DEP}/build.sh" $PWD || exit 1
+			done
+		fi
+		"${PKG}/build.sh" $PWD || exit 1
 	fi
 fi
+COMPILED=()
 PKGS=($ROOT/packages/*)
 for PKG in ${PKGS[*]}
 do
 	echo "Building ${PKG}..."
+	echo $PKG
 	if [[ ! -x "${PKG}/build.sh" ]] 
 	then
 		echo "Missing build script for ${PKG}!"
 	else
+		if [[ -f "${PKG}/DEPENDS" ]]
+		then
+			source "${PKG}/DEPENDS"
+			for DEP in "${DEPS[*]}" 
+			do
+				if [[ " ${COMPILED[@]} " =~ "${ROOT}/packages/${DEP}" ]]
+				then
+					return 
+				fi
+				if [[ ! -x "${ROOT}/packages/${DEP}/build.sh" ]]
+				then
+					echo -e "\e[91mMissing build script for ${DEP}"
+					exit 1
+				fi
+				"${ROOT}/packages/${DEP}/build.sh" $PWD || exit 1
+				COMPILED+=($ROOT/packages/$DEP)
+			done
+		fi
 		"${PKG}/build.sh" $PWD || exit 1
+		COMPILED+=($PKG)
 	fi
 done
