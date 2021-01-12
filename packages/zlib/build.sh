4b875d55208ebc54be0cf7e0068dd4f5d0d34c0e
Woomy4680-exe <matzar1808@gmail.com>
2021-01-12 14:02:39 +0100

    ⚡️ Update ZLIB pack script

--- packages/zlib/build.sh
+++ packages/zlib/build.sh
@@ -1,43 +1,19 @@
 #!/usr/bin/env bash
 ROOT=$1
+source "${ROOT}/libpkgs.sh"
 NAME="zlib"
 VER="1.2.11"
 EXT="tar.xz"
 URL="https://zlib.net/${NAME}-${VER}.${EXT}"
-if [[ ! -d "${ROOT}/temp" ]]
-then
-    exit 1
-fi
-if [[ -d "${ROOT}/temp/${NAME}" ]]
-then
-	rm -rf "${ROOT}/temp/${NAME}"
-fi
-mkdir "${ROOT}/temp/${NAME}"
 TMP="${ROOT}/temp/${NAME}"
-mkdir "${TMP}/sources"
-mkdir "${TMP}/build"
-ARCHIVE="${NAME}-${VER}.${EXT}"
-wget "${URL}" -O "${TMP}/${ARCHIVE}" || exit 1
-tar -xf "${TMP}/${ARCHIVE}" -C "${TMP}/sources" || exit 1
+config_pkg_dirs $NAME $ROOT
+download_and_unpack $URL $TMP $NAME $VER $EXT 
 SRC_DIR="${TMP}/sources/${NAME}-${VER}"
 cd ${SRC_DIR}
 ./configure --prefix=/usr
-make -j $(nproc)
-make install DESTDIR="${TMP}/build"
+make_and_install $TMP
 mkdir $TMP/build/lib
 mv -v $TMP/build/usr/lib/libz.so.* $TMP/build/lib/
 ln -sfv $TMP/build/lib/$(readlink $TMP/build/usr/lib/libz.so) $TMP/build/usr/lib/libz.s
-cd ${ROOT}
-# Define .PKG vars
-DATE=`date +"%D-%H:%M"`
-PACKAGER=$USER
-echo "name = \"${NAME}\"" | tee -a "${TMP}/build/.PKG"
-echo "version = $VER" | tee -a "${TMP}/build/.PKG"
-echo "subversion = 1" | tee -a "${TMP}/build/.PKG"
-echo "architecture = \"x64\"" | tee -a "${TMP}/build/.PKG"
-echo "packager = \"$USER\"" | tee -a "${TMP}/build/.PKG"
-echo "url = \"${URL}\"" | tee -a "${TMP}/build/.PKG"
-touch "${TMP}/build/.INSTALL"
-cd "${TMP}/build"
-tar -czf "${ROOT}/build/${NAME}-${VER}.tar.zst" .
-cd "${ROOT}"
+construct_PKG_file $NAME $VER $URL $TMP
+pack_zst "${TMP}/build" $NAME $VER $ROOT
