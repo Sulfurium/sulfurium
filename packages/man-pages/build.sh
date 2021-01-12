97829eb2ba9ed2a97d0d5688fdb08d82f8a8f7c8
Woomy4680-exe <matzar1808@gmail.com>
2021-01-12 13:50:15 +0100

    ⚡️ Man-pages script update

--- packages/man-pages/build.sh
+++ packages/man-pages/build.sh
@@ -1,38 +1,15 @@
 #!/usr/bin/env bash
 ROOT=$1
+source "${ROOT}/libpkgs.sh"
 NAME="man-pages"
 VER="5.08"
 EXT="tar.xz"
 URL="https://www.kernel.org/pub/linux/docs/${NAME}/${NAME}-${VER}.${EXT}"
-if [[ ! -d "${ROOT}/temp" ]]
-then
-    exit 1
-fi
-if [[ -d "${ROOT}/temp/${NAME}" ]]
-then
-	rm -rf "${ROOT}/temp/${NAME}"
-fi
-mkdir "${ROOT}/temp/${NAME}"
 TMP="${ROOT}/temp/${NAME}"
-mkdir "${TMP}/sources"
-mkdir "${TMP}/build"
-ARCHIVE="${NAME}-${VER}.${EXT}"
-wget "${URL}" -O "${TMP}/${ARCHIVE}" || exit 1
-tar -xf "${TMP}/${ARCHIVE}" -C "${TMP}/sources" || exit 1
+config_pkg_dirs $NAME $ROOT
+download_and_unpack $URL $TMP $NAME $VER $EXT 
 SRC_DIR="${TMP}/sources/${NAME}-${VER}"
 cd ${SRC_DIR}
 make install DESTDIR="${TMP}/build"
-cd ${ROOT}
-# Define .PKG vars
-DATE=`date +"%D-%H:%M"`
-PACKAGER=$USER
-echo "name = \"${NAME}\"" | tee -a "${TMP}/build/.PKG"
-echo "version = $VER" | tee -a "${TMP}/build/.PKG"
-echo "subversion = 1" | tee -a "${TMP}/build/.PKG"
-echo "architecture = \"x64\"" | tee -a "${TMP}/build/.PKG"
-echo "packager = \"$USER\"" | tee -a "${TMP}/build/.PKG"
-echo "url = \"${URL}\"" | tee -a "${TMP}/build/.PKG"
-touch "${TMP}/build/.INSTALL"
-cd "${TMP}/build"
-tar -czf "${ROOT}/build/${NAME}-${VER}.tar.zst" .
-cd "${ROOT}"
+construct_PKG_file $NAME $VER $URL $TMP
+pack_zst "${TMP}/build" $NAME $VER $ROOT
