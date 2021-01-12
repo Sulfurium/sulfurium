721c11459c76f84dc959b3030656708bcc434fc8
Woomy4680-exe <matzar1808@gmail.com>
2021-01-12 13:19:36 +0100

    ⚡️ Update expect pack script

--- packages/expect/build.sh
+++ packages/expect/build.sh
@@ -1,29 +1,13 @@
 #!/usr/bin/env bash
 ROOT=$1
+source "${ROOT}/libpkgs.sh"
 NAME="expect"
 VER="5.45.4"
 EXT="tar.gz"
 URL="https://prdownloads.sourceforge.net/${NAME}/${NAME}${VER}.${EXT}"
-if [[ ! -d "${ROOT}/temp" ]]
-then
-    exit 1
-fi
-if [[ -d "${ROOT}/temp/${NAME}" ]]
-then
-	rm -rf "${ROOT}/temp/${NAME}"
-fi
-mkdir "${ROOT}/temp/${NAME}"
 TMP="${ROOT}/temp/${NAME}"
-if [[ ! -d "${ROOT}/temp/tcl" ]]
-then
-	echo "You need to build TCL before!"
-	exit 1
-fi
-mkdir "${TMP}/sources"
-mkdir "${TMP}/build"
-ARCHIVE="${NAME}-${VER}.${EXT}"
-wget "${URL}" -O "${TMP}/${ARCHIVE}" || exit 1
-tar -xf "${TMP}/${ARCHIVE}" -C "${TMP}/sources" || exit 1
+config_pkg_dirs $NAME $ROOT
+download_and_unpack $URL $TMP $NAME $VER $EXT 
 SRC_DIR="${TMP}/sources/${NAME}${VER}"
 cd ${SRC_DIR}
 ./configure --prefix=/usr           \
@@ -31,20 +15,7 @@ cd ${SRC_DIR}
             --enable-shared         \
             --mandir=/usr/share/man \
             --with-tclinclude=${ROOT}/temp/tcl/build/usr/include
-make -j $(nproc)
-make install DESTDIR="${TMP}/build"
+make_and_install $TMP
 ln -svf libexpect5.45.4.so ${TMP}/build/usr/lib
-cd ${ROOT}
-# Define .PKG vars
-DATE=`date +"%D-%H:%M"`
-PACKAGER=$USER
-echo "name = \"${NAME}\"" | tee -a "${TMP}/build/.PKG"
-echo "version = $VER" | tee -a "${TMP}/build/.PKG"
-echo "subversion = 1" | tee -a "${TMP}/build/.PKG"
-echo "architecture = \"x64\"" | tee -a "${TMP}/build/.PKG"
-echo "packager = \"$USER\"" | tee -a "${TMP}/build/.PKG"
-echo "url = \"${URL}\"" | tee -a "${TMP}/build/.PKG"
-touch "${TMP}/build/.INSTALL"
-cd "${TMP}/build"
-tar -czf "${ROOT}/build/${NAME}-${VER}.tar.zst" .
-cd "${ROOT}"
+construct_PKG_file $NAME $VER $URL $TMP
+pack_zst "${TMP}/build" $NAME $VER $ROOT
