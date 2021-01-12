5738e6a7e3076867c758db308597a5db3a9fd2bb
Woomy4680-exe <matzar1808@gmail.com>
2021-01-12 13:29:34 +0100

    ⚡️ Update GCC packer

--- packages/gcc/build.sh
+++ packages/gcc/build.sh
@@ -1,24 +1,13 @@
 #!/usr/bin/env bash
 ROOT=$1
+source "${ROOT}/libpkgs.sh"
 NAME="gcc"
 VER="10.2.0"
 EXT="tar.xz"
 URL="http://ftp.gnu.org/gnu/gcc/gcc-${VER}/gcc-${VER}.tar.xz"
-if [[ ! -d "${ROOT}/temp" ]]
-then
-    exit 1
-fi
-if [[ -d "${ROOT}/temp/${NAME}" ]]
-then
-	rm -rf "${ROOT}/temp/${NAME}"
-fi
-mkdir "${ROOT}/temp/${NAME}"
 TMP="${ROOT}/temp/${NAME}"
-mkdir "${TMP}/sources"
-mkdir "${TMP}/build"
-ARCHIVE="${NAME}-${VER}.${EXT}"
-wget "${URL}" -O "${TMP}/${ARCHIVE}" || exit 1
-tar -xf "${TMP}/${ARCHIVE}" -C "${TMP}/sources" || exit 1
+config_pkg_dirs $NAME $ROOT
+download_and_unpack $URL $TMP $NAME $VER $EXT 
 SRC_DIR="${TMP}/sources/${NAME}-${VER}"
 cd ${SRC_DIR}
 case $(uname -m) in
@@ -35,26 +24,12 @@ cd       build
              --disable-multilib       \
              --disable-bootstrap      \
              --with-system-zlib
-make -j $(nproc)
-make install DESTDIR=$TMP/build
+make_and_install $TMP
 rm -rf $TMP/build/usr/lib/gcc/$(gcc -dumpmachine)/${VER}/include-fixed/bits/
 ln -sv ../usr/bin/cpp $TMP/build/lib
 install -v -dm755 /usr/lib/bfd-plugins
 ln -sfv ../../libexec/gcc/$(gcc -dumpmachine)/${VER}/liblto_plugin.so \
         $TMP/build/usr/lib/bfd-plugins/
 mkdir -pv $TMP/build/usr/share/gdb/auto-load/usr/lib
-mv -v $TMP/build/usr/lib/*gdb.py $TMP/build/usr/share/gdb/auto-load/usr/lib
-cd ${ROOT}
-# Define .PKG vars
-DATE=`date +"%D-%H:%M"`
-PACKAGER=$USER
-echo "name = \"${NAME}\"" | tee -a "${TMP}/build/.PKG"
-echo "version = $VER" | tee -a "${TMP}/build/.PKG"
-echo "subversion = 1" | tee -a "${TMP}/build/.PKG"
-echo "architecture = \"x64\"" | tee -a "${TMP}/build/.PKG"
-echo "packager = \"$USER\"" | tee -a "${TMP}/build/.PKG"
-echo "url = \"${URL}\"" | tee -a "${TMP}/build/.PKG"
-touch "${TMP}/build/.INSTALL"
-cd "${TMP}/build"
-tar -czf "${ROOT}/build/${NAME}-${VER}.tar.zst" .
-cd "${ROOT}"
+construct_PKG_file $NAME $VER $URL $TMP
+pack_zst "${TMP}/build" $NAME $VER $ROOT
