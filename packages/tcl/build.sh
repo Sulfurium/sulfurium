77708b92783d0fd7c6e279d37b4164ee9aed0430
Woomy4680-exe <matzar1808@gmail.com>
2021-01-12 13:59:38 +0100

    ⚡️ Update TCL pack

--- packages/tcl/build.sh
+++ packages/tcl/build.sh
@@ -1,23 +1,13 @@
 #!/usr/bin/env bash
 ROOT=$1
+source "${ROOT}/libpkgs.sh"
 NAME="tcl"
 VER="8.6.10"
 EXT="tar.gz"
 DOC_URL="https://downloads.sourceforge.net/${NAME}/${NAME}${VER}-html.${EXT}"
 URL="https://downloads.sourceforge.net/${NAME}/${NAME}${VER}-src.${EXT}"
-if [[ ! -d "${ROOT}/temp" ]]
-then
-    exit 1
-fi
-if [[ -d "${ROOT}/temp/${NAME}" ]]
-then
-	rm -rf "${ROOT}/temp/${NAME}"
-fi
-mkdir "${ROOT}/temp/${NAME}"
 TMP="${ROOT}/temp/${NAME}"
-mkdir "${TMP}/sources"
-mkdir "${TMP}/build"
-ARCHIVE="${NAME}${VER}-src.${EXT}"
+config_pkg_dirs $NAME $ROOT
 DOC_ARCHIVE="${NAME}${VER}-html.${EXT}"
 wget "${URL}" -O "${TMP}/${ARCHIVE}" || exit 1
 wget "${DOC_URL}" -O "${TMP}/${DOC_ARCHIVE}" || exit 1
@@ -31,7 +21,7 @@ cd unix
 ./configure --prefix=/usr           \
             --mandir=/usr/share/man \
             $([ "$(uname -m)" = x86_64 ] && echo --enable-64bit)
-make -j $(nproc)
+make $(get_make_opts)
 sed -e "s|$SRCDIR/unix|/usr/lib|" \
     -e "s|$SRCDIR|/usr/include|"  \
     -i tclConfig.sh
@@ -52,17 +42,6 @@ make DESTDIR="${TMP}/build" install
 chmod -v u+w ${TMP}/build/usr/lib/libtcl8.6.so
 make DESTDIR="${TMP}/build" install-private-headers
 ln -sv "tclsh8.6" "${TMP}/build/usr/bin/tclsh"
-cd ${ROOT}
-# Define .PKG vars
-DATE=`date +"%D-%H:%M"`
-PACKAGER=$USER
-echo "name = \"${NAME}\"" | tee -a "${TMP}/build/.PKG"
-echo "version = $VER" | tee -a "${TMP}/build/.PKG"
-echo "subversion = 1" | tee -a "${TMP}/build/.PKG"
-echo "architecture = \"x64\"" | tee -a "${TMP}/build/.PKG"
-echo "packager = \"$USER\"" | tee -a "${TMP}/build/.PKG"
-echo "url = \"${URL}\"" | tee -a "${TMP}/build/.PKG"
-touch "${TMP}/build/.INSTALL"
-cd "${TMP}/build"
-tar -czf "${ROOT}/build/${NAME}-${VER}.tar.zst" .
-cd "${ROOT}"
+construct_PKG_file $NAME $VER $URL $TMP
+pack_zst "${TMP}/build" $NAME $VER $ROOT
+
