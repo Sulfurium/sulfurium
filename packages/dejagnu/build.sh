7d3844f0d4ece79e6be46b19375ee6f5fb46da4c
Woomy4680-exe <matzar1808@gmail.com>
2021-01-12 13:18:16 +0100

    ⚡️ Update DejaGNU pack script

--- packages/dejagnu/build.sh
+++ packages/dejagnu/build.sh
@@ -1,43 +1,20 @@
 #!/usr/bin/env bash
 ROOT=$1
+source "${ROOT}/libpkgs.sh"
 NAME="dejagnu"
 VER="1.6.2"
 EXT="tar.gz"
 URL="http://ftp.gnu.org/gnu/${NAME}/${NAME}-${VER}.${EXT}"
-if [[ ! -d "${ROOT}/temp" ]]
-then
-    exit 1
-fi
-if [[ -d "${ROOT}/temp/${NAME}" ]]
-then
-	rm -rf "${ROOT}/temp/${NAME}"
-fi
-mkdir "${ROOT}/temp/${NAME}"
 TMP="${ROOT}/temp/${NAME}"
-mkdir "${TMP}/sources"
-mkdir "${TMP}/build"
-ARCHIVE="${NAME}-${VER}.${EXT}"
-wget "${URL}" -O "${TMP}/${ARCHIVE}" || exit 1
-tar -xf "${TMP}/${ARCHIVE}" -C "${TMP}/sources" || exit 1
+config_pkg_dirs $NAME $ROOT
+download_and_unpack $URL $TMP $NAME $VER $EXT 
 SRC_DIR="${TMP}/sources/${NAME}-${VER}"
 cd ${SRC_DIR}
 ./configure --prefix=/usr
 makeinfo --html --no-split -o doc/dejagnu.html doc/dejagnu.texi
 makeinfo --plaintext       -o doc/dejagnu.txt  doc/dejagnu.texi
 make install DESTDIR="${TMP}/build"
-install -v -dm755  ${TMP}/build/usr/share/doc/dejagnu-1.6.2
-install -v -m644   doc/dejagnu.{html,txt} ${TMP}/build/usr/share/doc/dejagnu-1.6.2
-cd ${ROOT}
-# Define .PKG vars
-DATE=`date +"%D-%H:%M"`
-PACKAGER=$USER
-echo "name = \"${NAME}\"" | tee -a "${TMP}/build/.PKG"
-echo "version = $VER" | tee -a "${TMP}/build/.PKG"
-echo "subversion = 1" | tee -a "${TMP}/build/.PKG"
-echo "architecture = \"x64\"" | tee -a "${TMP}/build/.PKG"
-echo "packager = \"$USER\"" | tee -a "${TMP}/build/.PKG"
-echo "url = \"${URL}\"" | tee -a "${TMP}/build/.PKG"
-touch "${TMP}/build/.INSTALL"
-cd "${TMP}/build"
-tar -czf "${ROOT}/build/${NAME}-${VER}.tar.zst" .
-cd "${ROOT}"
+install -v -dm755  ${TMP}/build/usr/share/doc/dejagnu-${VER}
+install -v -m644   doc/dejagnu.{html,txt} ${TMP}/build/usr/share/doc/dejagnu-${VER}
+construct_PKG_file $NAME $VER $URL $TMP
+pack_zst "${TMP}/build" $NAME $VER $ROOT
