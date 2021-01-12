81713d5e962af34517364e889e4080470ec5e005
Woomy4680-exe <matzar1808@gmail.com>
2021-01-12 13:54:50 +0100

    ⚡️ Update readline build script

--- packages/readline/build.sh
+++ packages/readline/build.sh
@@ -1,24 +1,13 @@
 #!/usr/bin/env bash
 ROOT=$1
+source "${ROOT}/libpkgs.sh"
 NAME="readline"
 VER="8.0"
 EXT="tar.gz"
 URL="http://ftp.gnu.org/gnu/readline/${NAME}-${VER}.${EXT}"
-if [[ ! -d "${ROOT}/temp" ]]
-then
-    exit 1
-fi
-if [[ -d "${ROOT}/temp/${NAME}" ]]
-then
-	rm -rf "${ROOT}/temp/${NAME}"
-fi
-mkdir "${ROOT}/temp/${NAME}"
 TMP="${ROOT}/temp/${NAME}"
-mkdir "${TMP}/sources"
-mkdir "${TMP}/build"
-ARCHIVE="${NAME}-${VER}.${EXT}"
-wget "${URL}" -O "${TMP}/${ARCHIVE}" || exit 1
-tar -xf "${TMP}/${ARCHIVE}" -C "${TMP}/sources" || exit 1
+config_pkg_dirs $NAME $ROOT
+download_and_unpack $URL $TMP $NAME $VER $EXT 
 SRC_DIR="${TMP}/sources/${NAME}-${VER}"
 cd ${SRC_DIR}
 sed -i '/MV.*old/d' Makefile.in
@@ -27,19 +16,7 @@ sed -i '/{OLDSUFF}/c:' support/shlib-install
             --disable-static \
             --with-curses    \
             --docdir=/usr/share/doc/readline-8.0
-make SHLIB_LIBS="-lncursesw" -j $(nproc)
+make SHLIB_LIBS="-lncursesw" $(get_make_args)
 make SHLIB_LIBS="-lncursesw" DESTDIR="$TMP/build" install
-cd ${ROOT}
-# Define .PKG vars
-DATE=`date +"%D-%H:%M"`
-PACKAGER=$USER
-echo "name = \"${NAME}\"" | tee -a "${TMP}/build/.PKG"
-echo "version = $VER" | tee -a "${TMP}/build/.PKG"
-echo "subversion = 1" | tee -a "${TMP}/build/.PKG"
-echo "architecture = \"x64\"" | tee -a "${TMP}/build/.PKG"
-echo "packager = \"$USER\"" | tee -a "${TMP}/build/.PKG"
-echo "url = \"${URL}\"" | tee -a "${TMP}/build/.PKG"
-touch "${TMP}/build/.INSTALL"
-cd "${TMP}/build"
-tar -czf "${ROOT}/build/${NAME}-${VER}.tar.zst" .
-cd "${ROOT}"
+construct_PKG_file $NAME $VER $URL $TMP
+pack_zst "${TMP}/build" $NAME $VER $ROOT
