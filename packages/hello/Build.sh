85fbce244f8c38c3d96a0e6111175d72479b228b
Woomy4680-exe <matzar1808@gmail.com>
2021-03-30 21:24:25 +0200

    Hello: Remove useless debug infos

--- packages/hello/Build.sh
+++ packages/hello/Build.sh
@@ -4,12 +4,11 @@ NAME="hello"
 DESCRIPTION="GNU hello world implementation"
 VERSION="2.10"
 SRC=("https://ftp.nluug.nl/pub/gnu/hello/hello-${VERSION}.tar.gz")
-HOMEPAGE="https://www.gnu.org/software/hello/"
+HOMEPAGE="https://www.gnu.org/software/${NAME}/"
 extract() {
 	tar xf "${NAME}-${VERSION}.tar.gz"
 }
 prepare() {
-	pwd
 	./configure --prefix=/usr
 }
 
